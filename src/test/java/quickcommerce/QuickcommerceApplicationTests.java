09a4452043e1476b0f2db8333db88b25c744223f
Abhiram Dhulipalla <abhiram@Abhirams-MacBook-Air.local>
2026-05-20 23:37:11 +0530

    Initialize Spring Boot project

---
+++ src/test/java/quickcommerce/QuickcommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package quickcommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuickcommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
